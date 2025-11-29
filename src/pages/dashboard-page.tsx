6118bc9c88e5ba1c252d175632d101574aa323e6
Keshav Kishor Ram <ramabishek40@gmail.com>
2025-11-29 16:38:37 +0545

    style: minor responsiveness changes

--- src/pages/dashboard-page.tsx
+++ src/pages/dashboard-page.tsx
@@ -6,12 +6,12 @@ import { PlusIcon } from 'lucide-react';
 const DashboardPage = () => {
   return (
     <div className="flex flex-col gap-5">
-      <div className="flex items-start justify-between">
+      <div className="flex flex-col items-start justify-between gap-y-4 sm:flex-row">
         <div className="flex flex-col">
           <h2 className="text-2xl font-bold text-fg-primary">Projects</h2>
           <p>Manage and track your projects</p>
         </div>
-        <Button>
+        <Button className="ml-auto">
           <PlusIcon />
           Create Project
         </Button>
